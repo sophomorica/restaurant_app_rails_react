36f64fd6b720eb758683fb976d33ecb8cc78114a
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-10 14:19:09 -0600

    built out controllers

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,2 +1,31 @@
 class Api::ItemsController < ApplicationController
+  def index
+    item = Item.all
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item
+    else
+      render json: { errors: item.errors }, status: :unprocessable_entity 
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update
+    render json: item
+  end
+
+  def destroy
+    Item.find(params[:id]).destroy
+    render json: {message: 'Item deleted'}
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :complete)
+  end
 end
--- app/controllers/api/menus_controller.rb
+++ app/controllers/api/menus_controller.rb
@@ -1,2 +1,32 @@
 class Api::MenusController < ApplicationController
+
+  def index
+    render json: Menu.all
+  end
+
+  def create
+    menu = Menu.new(menu_params)
+    if menu.save
+      render json: menu
+    else
+      render json: {errors: menu.errors}, status: :unprocessable_entitiy
+    end
+  end
+
+  def update
+    menu = Menu.find(params[:id])
+    menu.update
+    render json: item
+  end
+
+  def destroy
+    Menu.find(params[:id]).destroy
+    render json: { message: 'Menu Deleted'}
+  end
+
+  private
+  def menu_params
+    params.require(:menu).permit(:name)
+  end
+  
 end
